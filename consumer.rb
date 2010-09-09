cf0224d1dc4f725524b694205a1edbf7a0936533
Tony Day <tony@lazylabs.com>
2010-09-09 14:05:04 +1200

    Some mother comment.

--- consumer.rb
+++ consumer.rb
@@ -21,6 +21,7 @@ AMQP.start do
     print "#{n} "
     File.open(foo, 'a') { |file| file.puts "#{n}: #{body}" } if body
     # If we do not ack the message it will not be purged from the queue
+    # blah blah blah
 #    header.ack
   end
 
