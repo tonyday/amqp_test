784d7629d0d5f67844db5aac9fb44d2b870c0221
Tony Day <tony@lazylabs.com>
2010-09-17 15:31:56 +1200

    Some more baby steps.

--- consumer.rb
+++ consumer.rb
@@ -2,27 +2,38 @@
 
 require 'mq'
 
-Signal.trap('INT') { AMQP.stop{ EM.stop } }
-Signal.trap('TERM'){ AMQP.stop{ EM.stop } }
+#Signal.trap('INT') { AMQP.stop { EM.stop } }
+#Signal.trap('TERM') { AMQP.stop { EM.stop } }
 
 $stdout.sync = true
 
 foo = 'foo'
 
-File.delete(foo) if File.exist?(foo)
+#File.delete(foo) if File.exist?(foo)
 
 AMQP.start do
-  q = MQ.queue('qname')
+  q = MQ.queue('td_queue')
+
+  MQ.prefetch(1)
 
   n = 0
   q.subscribe(:ack => true) do |header, body|
 #  q.subscribe() do |msg|
     n += 1
     print "#{n} "
-    File.open(foo, 'a') { |file| file.puts "#{n}: #{body}" } if body
+#    File.open(foo, 'a') { |file| file.puts "#{n}: #{body}" } if body
+    puts "#{n}: #{body}" if body
     # If we do not ack the message it will not be purged from the queue
     # blah blah blah
-#    header.ack
+    header.ack
+    if n == 10
+      puts 'That is enough for now'
+      q.unsubscribe
+      AMQP.stop do
+        EM.stop
+      end
+    end
+    sleep rand * 3
   end
 
 end
--- publisher.rb
+++ publisher.rb
@@ -8,15 +8,17 @@ AMQP.start do
 # accessed. Make sure this is the same queue our consumers
 # will use.
 #  qname = Time.now.strftime('%Y%m%d%H%M%S')
-  qname = 'qname'
-  q = MQ.queue(qname, :durable => true)
+  exchange_name = 'td_exchange'
+  queue_name = 'td_queue'
+  exchange = MQ.direct(exchange_name, :durable => true)
+  MQ.queue(queue_name, :durable => true).bind(exchange_name)
 
   i=0
   while i < 1000
     i += 1
-    d = "Hello, World #{qname} - #{i}"
+    d = "Hello, World #{exchange_name} - #{i}"
     p d
-    q.publish(d, :persistent => true)
+    exchange.publish(d, :persistent => true)
   end
 
   AMQP.stop{ EM.stop }
